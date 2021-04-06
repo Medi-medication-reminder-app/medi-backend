847c8d92b2915df1efa8a477549ce448a217d837
Adrian Scrab <adrian.scrab@pm.me>
2021-04-06 17:48:56 +0300

    remove duplicate auth code

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -4,6 +4,15 @@ use crate::models::api::{signup::SignupForm, login::LoginForm};
 use crate::DbConn;
 use crate::utils::response::*;
 
+fn _login(data: LoginForm, conn: &DbConn) -> Result<ApiResponse, ApiError> {
+    let jwt = LoginForm::login(data, &conn);
+    match jwt {
+        Ok(Some(t)) => Ok(success(json!(t))),
+        Ok(None) => Err(fail(403, String::from("Forbidden"), String::from("Invalid password"))),
+        Err(e) => Err(db_error(e)),
+    }
+}
+
 #[post("/signup", data = "<form>")]
 fn signup(
     form: Result<Json<SignupForm>, JsonError>,
@@ -16,14 +25,7 @@ fn signup(
             };
             let result = SignupForm::create(insert, &conn);
             match result {
-                Ok(r) => {
-                    let jwt = LoginForm::login(r, &conn);
-                    match jwt {
-                        Ok(Some(t)) => Ok(success(json!(t))),
-                        Ok(None) => Err(fail(403, String::from("Forbidden"), String::from("Invalid password"))),
-                        Err(e) => Err(db_error(e)),
-                    }
-                },
+                Ok(r) => _login(r, &conn),
                 Err(e) => Err(db_error(e)),
             }
         }
@@ -41,12 +43,7 @@ fn login(
             let login_info = LoginForm {
                 ..f.into_inner()
             };
-            let result = LoginForm::login(login_info, &conn);
-            match result {
-                Ok(Some(r)) => Ok(success(json!(r))),
-                Ok(None) => Err(fail(403, String::from("Forbidden"), String::from("Invalid password"))),
-                Err(e) => Err(db_error(e)),
-            }
+            _login(login_info, &conn)
         }
         Err(e) => Err(json_error(e)),
     }
